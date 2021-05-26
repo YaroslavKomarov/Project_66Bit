61113dec26d365d8fb305c64b9510ab0b196028c
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-05-26 18:28:24 +0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_66_bit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
