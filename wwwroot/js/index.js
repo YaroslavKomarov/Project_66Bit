10a6ff113ebb8aa3bacf3adf4870597b5cc8773d
Danila Kuntsevich <pro_voda@bk.ru>
2021-06-12 22:30:14 +0500

    Add pages

---
+++ wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var button = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+button.onclick = function () {
+    modal.style.display = "none";
+}
