52dd3699c057cee65fed7c85581a166f9712ad50
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-06-16 16:33:45 +0500

    Minor fix

--- Pages/Mod.cshtml.cs
+++ Pages/Mod.cshtml.cs
@@ -1,11 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project_66_bit.Models;
-using ASPNET_MVC.Models;
 
 namespace RazorProject.Pages
 {
