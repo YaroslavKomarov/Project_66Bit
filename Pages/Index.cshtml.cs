3ffb4e476ade8430556e1ffdabec8f3c0fc3b7ca
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-06-20 15:27:11 +0500

    Commit before pulling

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,38 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Project_66_bit.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorProject.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ApplicationDbContext db;
-        public List<Project> Projecte { get; set; }
-        public Project Person { get; set; }
+        private readonly ApplicationDbContext _context;
+        public List<Project> Projects { get; set; }
+        public List<Module> Modules { get; set; }
+        public List<Customer> Customers { get; set; }
+        [BindProperty]
+        public Project NewProject { get; set; }
+        [BindProperty]
+        public Customer NewCustomer { get; set; }
 
-        public IndexModel(ApplicationDbContext logger)
+        public IndexModel(ApplicationDbContext db)
         {
-            db = logger;
+            _context = db;
+            NewProject = new Project();
+            NewCustomer = new Customer();
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            Projecte = db.Projects.ToList();
+            Projects = await _context.Projects.ToListAsync();
+            Modules = await _context.Modules.ToListAsync();
+            Customers = await _context.Customers.ToListAsync();
+
+            Projects.Reverse();
+            Modules.Reverse();
+            Customers.Reverse();
         }
+
         public async Task<IActionResult> OnPostAsync()
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                db.Projects.Add(Person);
-                await db.SaveChangesAsync();
-                return RedirectToPage("Index");
+                return Page();
             }
-            return Page();
-        }
+
+            await _context.Customers.AddAsync(NewCustomer);
+            await _context.SaveChangesAsync();
+
+            NewProject.Customer = await _context.Customers
+                .OrderByDescending(t => t.Id)
+                .FirstOrDefaultAsync();
+
+            await _context.Projects.AddAsync(NewProject);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }    
     }
 }
--- Pages/Mod.cshtml.cs
+++ Pages/Mod.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Project_66_bit.Models;
 
 namespace RazorProject.Pages
@@ -9,14 +12,56 @@ namespace RazorProject.Pages
     public class ModModel : PageModel
     {
         private readonly ApplicationDbContext _context;
-        public List<Project> Task { get; set; }
+        public Project Project { get; set; }
+        public Customer Customer { get; set; }
+        public List<Module> Modules { get; set; }
+        public List<Problem> Problems { get; set; }
+        [BindProperty]
+        public Module NewModule { get; set; }
+        [BindProperty]
+        public Problem NewProblem { get; set; }
+
         public ModModel(ApplicationDbContext db)
         {
             _context = db;
         }
-        public void OnGet()
+
+        public async Task OnGetAsync(int id)
         {
-            Task = _context.Projects.ToList();
+            Project = await _context.Projects.FindAsync(id);
+            Customer = await _context.Customers.FindAsync(Project.CustomerId);
+            Modules = await _context.Modules.Where(m => m.ProjectId == id).ToListAsync();
+            Problems = await _context.Problems.ToListAsync();
+
+            Modules.Reverse();
+        }
+
+        public async Task<IActionResult> OnPostModuleAsync(int id)
+        {
+            if (NewModule == null)
+            {
+                return Page();
+            }
+
+            NewModule.Project = await _context.Projects.FindAsync(id);
+            await _context.Modules.AddAsync(NewModule);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Mod", new { id = id });
+        }
+
+        public async Task<IActionResult> OnPostProblemAsync(int id, int projId)
+        {
+            if (NewProblem == null)
+            {
+                return Page();
+            }
+
+            NewProblem.Module = await _context.Modules.FindAsync(id);
+            await _context.Problems.AddAsync(NewProblem);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Mod", new { id = projId });
         }
     }
 }
