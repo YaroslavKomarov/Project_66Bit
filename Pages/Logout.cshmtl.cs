79bae89ada72f38a9669a92da152b3546983c818
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-06-20 22:08:31 +0500

    Logout added

---
+++ Pages/Logout.cshmtl.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorProject.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/Enter");
+        }
+    }
+}
