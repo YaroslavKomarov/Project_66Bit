f862f91a5b5194e1f32a38960f3bb8cdbe8f05e4
Yaroslav Komarov <yaroslav.komarov@yandex.ru>
2021-07-08 20:58:58 +0500

    up

--- wwwroot/js/alertMssg.js
+++ wwwroot/js/alertMssg.js
@@ -1,34 +1,9 @@
-﻿let deleteProj = document.getElementById(deleteProjId);
-let deleteMod = document.getElementById(deleteProjId);
-let deleteProblem = document.getElementById(deleteProjId);
-let confirmSpaceProj = document.querySelector('.confirm-space-proj');
-let confirmSpaceMod = document.querySelector('.confirm-space-mod');
-let confirmSpaceProblem = document.querySelector('.confirm-space-problem');
-
-let cancelMod = document.getElementById(cancelModId);
-let cancelProj = document.getElementById(cancelProjId);
-let cancelProblem = document.getElementById(cancelProblemId);
+﻿confirmSpaceProj.style.display = "none";
 
 deleteProj.onclick = function () {
-    confirmSpaceProj.style.display = 'block';
-}
-
-deleteMod.onclick = function () {
-    confirmSpaceMod.style.display = 'block';
-}
-
-deleteProblem.onclick = function () {
-    confirmSpaceMod.style.display = 'block';
+    confirmSpaceProj.style.display = "block";
 }
 
 cancelProj.onclick = function () {
-    confirmSpaceProj.style.display = 'none';
-}
-
-cancelMod.onclick = function () {
-    confirmSpaceMod.style.display = 'none';
-}
-
-cancelProblem.onclick = function () {
-    confirmSpaceProblem.style.display = 'none';
+    confirmSpaceProj.style.display = "none";
 }
