3454bb1fd541b933e04d4ac0d6128b0705eaa54c
Yaroslav Komarov <yaroslav.komarov@yandex.ru>
2021-07-16 21:08:35 +0500

    tmp

--- wwwroot/js/mainSearch.js
+++ wwwroot/js/mainSearch.js
@@ -1,14 +1,22 @@
 let projectParent = document.querySelector(".project");
+let moduleParent = document.querySelector(".module");
 let projects = [...projectParent.children];
+let modules = [...moduleParent.children];
 let searchInput = document.querySelector(".search-input");
 let projectNames = [];
+let moduleNames = [];
 
 for (let i = 0; i < projects.length; i++) {
     projectNames.push(projects[i].children[0].children[1].text.toLowerCase());
 }
 
+for (let i = 0; i < modules.length; i++) {
+    moduleNames.push(modules[i].children[0].children[1].text.toLowerCase());
+}
+
 searchInput.oninput = function () {
     projectParent.innerHTML = '';
+    moduleParent.innerHTML = '';
     let flag = true;
     if (searchInput.value != "") {
         for (let i = 0; i < projectNames.length; i++) {
