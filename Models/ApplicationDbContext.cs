6c08f0fb30f01b21d3ac6da8309213b625f947a7
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-07-17 21:19:13 +0500

    Logging added

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Project_66_bit.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        private readonly StreamWriter logStream = new StreamWriter("DBLog.txt", true);
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Module> Modules { get; set; }
@@ -13,5 +18,22 @@ namespace Project_66_bit.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(logStream.WriteLine, LogLevel.Information);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            logStream.Dispose();
+        }
+ 
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logStream.DisposeAsync();
+        }
     }
 }
